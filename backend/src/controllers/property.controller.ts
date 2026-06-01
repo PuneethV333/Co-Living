9562d0666bad83cc83892e4dee0628d0d2465a19
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-01 23:30:24 +0530

    added-api-for-details

--- backend/src/controllers/property.controller.ts
+++ backend/src/controllers/property.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { getError } from "../utils/error.utils"
-import { getPropertyDataService } from "../services/property.services"
+import { getPropertyDataService, getPropertyDetailService } from "../services/property.services"
 
 export const getPropertyData = async (req: Request, res: Response) => {
     try {
@@ -13,6 +13,33 @@ export const getPropertyData = async (req: Request, res: Response) => {
         }
 
         const result = await getPropertyDataService(firebaseUid)
+
+        return res.status(200).json({
+            data: result.data,
+            source: result.source
+        })
+    } catch (err) {
+        res.status(500).json(getError(err))
+    }
+}
+export const getPropertyDetails = async (req: Request, res: Response) => {
+    try {
+        const firebaseUid = req.user?.firebaseUid
+
+        if (!firebaseUid) {
+            return res.status(400).json({
+                message: "unauthorized"
+            })
+        }
+        
+        const propertyId = req.params.id
+        if(!propertyId){
+            return res.status(400).json({
+                message: "Data not provided"
+            })
+        }
+
+        const result = await getPropertyDetailService(firebaseUid,Array.isArray(propertyId)?propertyId[0]:propertyId)
 
         return res.status(200).json({
             data: result.data,
--- backend/src/services/property.services.ts
+++ backend/src/services/property.services.ts
@@ -41,3 +41,22 @@ export const getPropertyDataService = async (
     };
 };
 
+
+export const getPropertyDetailService = async (firebaseUid:string,propertyId:string) => {
+    const cacheKey = `property:${propertyId}:${firebaseUid}`
+    const cached = await getVal(cacheKey);
+    if(cached){
+        return {data:JSON.parse(cached),source:"redis"}
+    }
+    
+    const user = await User.findOne({firebaseUid}).lean()
+    
+    if(!user){
+        throw new Error("Unauthorized")
+    }
+    
+    const data = await Property.findOne({_id:propertyId}).populate("ownerId","name phoneNumber verified").lean()
+    
+    await setValKey(cacheKey,JSON.stringify(data))
+    return {data,source:"db"}
+}
