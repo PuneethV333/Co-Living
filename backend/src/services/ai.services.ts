f5d0457091f050daf5b2deb48bede5e65b5fdce1
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-07 16:54:15 +0530

    fix:moved-from-openai-to-genai

--- backend/src/services/ai.services.ts
+++ backend/src/services/ai.services.ts
@@ -1,38 +1,12 @@
-// import axios from "axios";
-// import { config } from "../config/data.config";
-
 import { aiClient } from "../config/genAi.config";
 
-// export const getEmbeddingServices = async (
-//     text: string
-// ): Promise<number[]> => {
-//     try {
-//         const response = await axios.post(
-//             config.ollamUrl,
-//             {
-//                 model: "nomic-embed-text",
-//                 prompt: text,
-//             }
-//         );
-
-//         return response.data.embedding;
-//     } catch (err: any) {
-//         console.error(
-//             "Embedding Error:",
-//             err.response?.data || err.message
-//         );
-//         throw err;
-//     }
-// };
-
-
 export const getEmbeddingServices = async (
-  text: string
+    text: string
 ): Promise<number[]> => {
-  const response = await aiClient.models.embedContent({
-    model: "gemini-embedding-001",
-    contents: text,
-  });
+    const response = await aiClient.models.embedContent({
+        model: "gemini-embedding-001",
+        contents: text,
+    });
 
-  return response.embeddings![0].values!;
+    return response.embeddings![0].values!;
 };
