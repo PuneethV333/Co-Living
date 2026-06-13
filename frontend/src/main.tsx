4f6ca1620c449834c02cd7e27e3b03fbc7db55d2
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-13 20:48:34 +0530

    add:vercel.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import UiProvider from "./context/UiContext.tsx";
 
 const queryClient = new QueryClient({
@@ -22,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
         <UiProvider>
           <App />
         </UiProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
